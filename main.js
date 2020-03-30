c6fab4ab147c4614fdb4dbb39a0c5d511e89aacf
RobertJunas <rob.junas@gmail.com>
2020-03-30 12:58:39 +0200

    Ovladanie setUp

--- main.js
+++ main.js
@@ -8,12 +8,17 @@ var states = [
         new GameButton(()=>{stateManager.change = 1},
                         {position : new vec2(-0.5,0.1),scale : new vec2(5,2),fillColor : "red",strokeColor : "white", lineWidth : 3},
                         {message : "Play",font : "Orbitron",size : "30px"}),
-        new GameButton(()=>{},
+        new GameButton(()=>{stateManager.change = 2},
                         {position : new vec2(-0.5,-0.15),scale : new vec2(5,1.5),fillColor : "red",strokeColor : "white", lineWidth : 3},
                         {message : "Controls",font : "Orbitron",size : "20px"}),
         new GameButton((button)=>{
+                        if(button.text.text.includes("ON")){
                         states[0].objects[5].image.src= "./res/soundOFF.png";
                         button.text.text = "sound OFF"
+                        }else{
+                            states[0].objects[5].image.src= "./res/sound.png";
+                            button.text.text = "sound ON"  
+                        }
                         },
                         {position : new vec2(-0.5,-0.35),scale : new vec2(5,1.5),fillColor : "red",strokeColor : "white", lineWidth : 3},
                         {message : "sound ON",font : "Orbitron",size : "20px"}),
@@ -33,9 +38,18 @@ var states = [
     //ovladanie
     new State([
         new GameImage("./res/background.jpg",new vec2(0,0),800,600),
-        new GameImage("./res/",new vec2(0,0),800,600),
-        new GameImage("./res/background.jpg",new vec2(0,0),800,600),
+        new GameButton(()=>{stateManager.change = 0},
+                        {position : new vec2(0,-0.7), scale: new vec2(5,2),fillColor : "red",strokeColor : "white", lineWidth : 3},
+                        {message : "Back to menu",font : "Orbitron",size : "20px"})
 
+    ]),
+    new State([
+        new GameImage("./res/background.jpg",new vec2(0,0),800,600),
+        new GameText("GAME OVER", new vec2(0,0.6),
+                {font : "Orbitron", size : "100px"}),
+        new GameButton(()=>{stateManager.change = 0},
+                {position : new vec2(-0.5,-0.7), scale: new vec2(5,2),fillColor : "red",strokeColor : "white", lineWidth : 3},
+                {message : "Back to menu",font : "Orbitron",size : "20px"})
     ])
 ]
 var stateManager = new StateManager();
@@ -43,7 +57,7 @@ states.forEach(e =>{
     stateManager.addState = e;
 })
 
-stateManager.change = 0;
+stateManager.change = 3;
 
 var area = new GameArea("area",3/4,800,stateManager);
 var controller = document.createElement('game-controller');
